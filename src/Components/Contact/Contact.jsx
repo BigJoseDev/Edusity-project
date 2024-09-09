47dbcfeb65b5d22acb7d5e3b14854a7371518167
Jose <aliemhejosemaria@gmail.com>
2024-09-09 01:49:15 +0100

    stil on media query

--- src/Components/Contact/Contact.jsx
+++ src/Components/Contact/Contact.jsx
@@ -15,7 +15,7 @@ const Contact = () => {
             <h3>Send us a message <img src={msg_icon} alt="" /></h3>
             <p>Feel free to reach out to through contact form or find our contact information below. Your feedback, questions, and sugestions are important to us as we strive to provide exceptional service to our university community.</p>
             <ul>
-                <li><img src={mail_icon} alt="" />Josemaria14.workgmail.com </li>
+                <li><img src={mail_icon} alt="" />Josemaria14.work@gmail.com </li>
                 <li><img src={phone_icon} alt="" />+234 70499924540 </li>
                 <li><img src={location_icon} alt="" />Lagos Nigeria </li>
             </ul>
