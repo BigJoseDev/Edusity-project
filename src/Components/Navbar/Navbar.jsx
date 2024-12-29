710f452d7fcc16428a97c25c0c6e80b7f8d65599
Jose <aliemhejosemaria@gmail.com>
2024-12-29 22:57:56 +0100

    used edusity logo

--- src/Components/Navbar/Navbar.jsx
+++ src/Components/Navbar/Navbar.jsx
@@ -5,6 +5,7 @@ import menu_icon from '../../assets/menu-icon.png'
 import { Link } from 'react-scroll';
 
 
+
 const Navbar = () => {
 
   const [sticky, setSticky] = useState(false);
@@ -23,8 +24,8 @@ const Navbar = () => {
   
   return (
    <nav className={`container ${sticky? 'dark-nav' : ''} `} >
-    {/* <img src={logo} alt="" className='logo' /> */}
-    <h1 className='logo-name'>Learning Academy</h1>
+    <img src={logo} alt="" className='logo' />
+    {/* <h1 className='logo-name'>Learning Academy</h1> */}
     <ul className={mobileMenu? '' : 'hide-mobile-menu'}>
         <li><Link to='hero' smooth={true} offset={0} duration={500}>Home</Link></li>
         <li><Link to='program' smooth={true} offset={-260} duration={500}>Program</Link></li>
